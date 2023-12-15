99670cde9e1147fd4938913b4d740878f0aa0a5e
VOPNO <beserraguilherme405@gmail.com>
2023-12-15 17:59:56 -0300

    new fase

---
+++ logica/aula1/operadores.js
@@ -0,0 +1,12 @@
+/* 
+Operadores de comparação
+> Maior que
+>= Maior que ou igual a 
+< menor que
+<= menor qu ou igual a 
+== igualdade 
+=== igualdade estrita
+!= diferente
+!== diferença estrita
+*/
+
